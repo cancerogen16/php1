9e22b87fdae28ede3d405e7d317ac341fcdde8e2
cancerogen-ge <don544@mail.ru>
2021-02-27 15:53:53 +0500

    Решение домашнего задания №1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php
+$title = 'Первый урок';
+$head = 'Заголовок страницы';
+$year = 2021;
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+</head>
+
+<body>
+    <h1><?=$head?></h1>
+    <p>Текущий год: <?=$year?></p>
+</body>
+
+</html>
