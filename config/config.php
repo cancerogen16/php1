b0926cacbb93199f253c03e4aa2a5878d0eb8a5a
cancerogen-ge <don544@mail.ru>
2021-03-19 02:47:57 +0500

    config

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('basePath', dirname(__DIR__));
+define('PUBLIC_DIR', basePath . '/public');
+define('ADMIN_DIR', basePath . '/public/admin');
+define('ENGINE_DIR', basePath . '/engine');
+define('UPLOADS_DIR', basePath . '/uploads');
+define('TEMPLATES_DIR', basePath . '/templates');
+define('IMAGES_DIR', PUBLIC_DIR . '/img');
