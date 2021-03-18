016bc2187669ce31862f6be5585df2e5f6e2c5c3
cancerogen-ge <don544@mail.ru>
2021-03-19 01:41:13 +0500

    Нет изображения

---
+++ public/admin/catalog.php
@@ -0,0 +1,97 @@
+<?php
+require __DIR__ . '/../../config/config.php';
+
+require_once(ENGINE_DIR . '/functions.php');
+require_once(ENGINE_DIR . '/db_model.php');
+
+$message = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $message = uploadImage();
+}
+
+$products = getProducts();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Редактирование каталога товаров</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <?php require_once(TEMPLATES_DIR . '/admin/header.php'); ?>
+    <hr>
+
+    <div class="content">
+        <div class="container">
+            <h1>Редактирование каталога товаров</h1>
+        </div>
+        <hr>
+        <div class="container">
+            <div class="products">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>№</th>
+                            <th>Наименование</th>
+                            <th>Изображение</th>
+                            <th>Цена</th>
+                            <th>Количество</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (!empty($products)) : ?>
+                        <?php foreach ($products as $p => $product) : ?>
+                        <tr>
+                            <td><?= ($p + 1) ?></td>
+                            <td><?= $product['name'] ?></td>
+                            <td>
+                                <?php if (!empty($product['image'])) : ?>
+                                <img class="product-image" src="/img/<?= $product['image'] ?>"
+                                    alt="<?= $product['name'] ?>" width="40">
+                                <?php else : ?>
+                                Нет изображения
+                                <?php endif; ?>
+                            </td>
+                            <td><?= $product['price'] ?></td>
+                            <td><?= $product['quantity'] ?></td>
+                            <td><a href="product.php?product_id=<?= $product['product_id'] ?>"
+                                    title="Редактировать">Редактировать</a></td>
+                            <td><a href="product.php?product_id=<?= $product['product_id'] ?>"
+                                    title="Удалить">Удалить</a></td>
+                        </tr>
+                        <?php endforeach; ?>
+                        <?php else : ?>
+                        <tr>
+                            <td colspan="10">Нет товаров</td>
+                        </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+
+                <div class="add-product">
+                    <a href="product.php?add_product=1" title="Добавить товар">Добавить
+                        товар</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+    <?php require_once(TEMPLATES_DIR . '/admin/footer.php'); ?>
+
+    <?php require_once(TEMPLATES_DIR . '/modal.php'); ?>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
