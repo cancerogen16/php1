784ca6ccb28e5142fffffba4570aed3f4280fff9
cancerogen-ge <don544@mail.ru>
2021-03-19 17:40:54 +0500

    Урок 6. Интерактивность: Методы передачи данных GET и POST, работа с формами и пользовательскими данными

--- public/index.php
+++ public/index.php
@@ -8,7 +8,8 @@ require __DIR__ . '/../config/config.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 5. Базы данных MySQL и работа с ними на уровне PHP</title>
+    <title>Урок 6. Интерактивность: Методы передачи данных GET и POST, работа с формами и пользовательскими данными
+    </title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 
