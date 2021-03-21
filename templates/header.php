7d7e4a9c43195fd5b3ff0864e5935de70a76ef55
cancerogen-ge <don544@mail.ru>
2021-03-22 01:47:49 +0500

    require_once __DIR__ . '/../config/config.php';

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 
 require_once(ENGINE_DIR . '/functions.php');
 require_once(ENGINE_DIR . '/db_model.php');
