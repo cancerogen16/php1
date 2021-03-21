3656e4d4cea74514387b1a9d67cf715387c68096
cancerogen-ge <don544@mail.ru>
2021-03-22 02:46:37 +0500

    session_start();

--- public/catalog.php
+++ public/catalog.php
@@ -6,6 +6,8 @@ ini_set('error_reporting', (string)E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
+session_start();
+
 require __DIR__ . '/../config/config.php';
 
 require_once(ENGINE_DIR . '/functions.php');
