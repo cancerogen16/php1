a271c6be474a505e86c5cfd607392008609f8c2f
cancerogen-ge <don544@mail.ru>
2021-03-22 15:37:36 +0500

    $cart_products = $cart['products'];

--- templates/cart.php
+++ templates/cart.php
@@ -5,20 +5,30 @@ if (isset($_SESSION["user_id"]) && $_SESSION["user_id"]) {
     $user_id = (int)$_SESSION["user_id"];
 }
 
-$products = [];
+$product_id = filter_input(INPUT_GET, 'product_id', FILTER_SANITIZE_SPECIAL_CHARS);
+
+$addToCart = filter_input(INPUT_GET, 'addToCart', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if ($product_id) {
+    if ($addToCart) {
+        addToCart($product_id, $user_id);
+    }
+}
+
+$cart_products = [];
 
 if ($cart = getCart($user_id)) {
-    $products = $cart['products'];
+    $cart_products = $cart['products'];
 }
 ?>
 
 <div class="header-cart">
     <a class="cart__link" href="/cart.php">Корзина (<?= $cart['count'] ?>)</a>
-    <?php if (!empty($products)) : ?>
+    <?php if (!empty($cart_products)) : ?>
     <div class="cart-content">
         <table>
             <tbody>
-                <?php foreach ($products as $product) : ?>
+                <?php foreach ($cart_products as $product) : ?>
                 <tr>
                     <td><img src="img/<?= $product['image'] ?>" alt="<?= $product['name'] ?>" width="24"></td>
                     <td><a class="cart__link"
