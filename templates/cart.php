a42fc586342322ea4f050246660f3487e1571978
cancerogen-ge <don544@mail.ru>
2021-03-22 15:23:32 +0500

    <td colspan="3">Итого</td>
                        <td><?= $cart['count'] ?></td>
                        <td class="price"><?= $cart['total'] ?></td>

--- templates/cart.php
+++ templates/cart.php
@@ -1,5 +1,43 @@
-<?php if (1) : ?>
-<div class="cart">
-    <a class="cart__link" href="/cart.php">Корзина</a>
+<?php
+$user_id = 0;
+
+if (isset($_SESSION["user_id"]) && $_SESSION["user_id"]) {
+    $user_id = (int)$_SESSION["user_id"];
+}
+
+$products = [];
+
+if ($cart = getCart($user_id)) {
+    $products = $cart['products'];
+}
+?>
+
+<div class="header-cart">
+    <a class="cart__link" href="/cart.php">Корзина (<?= $cart['count'] ?>)</a>
+    <?php if (!empty($products)) : ?>
+    <div class="cart-content">
+        <table>
+            <tbody>
+                <?php foreach ($products as $product) : ?>
+                <tr>
+                    <td><img src="img/<?= $product['image'] ?>" alt="<?= $product['name'] ?>" width="24"></td>
+                    <td><a class="cart__link"
+                            href="/product.php?product_id=<?= $product['product_id'] ?>"><?= $product['name'] ?></a>
+                    </td>
+                    <td class="price"><?= $product['price'] ?></td>
+                    <td><?= $product['quantity'] ?></td>
+                    <td class="price"><?= $product['total'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3">Итого</td>
+                    <td><?= $cart['count'] ?></td>
+                    <td class="price"><?= $cart['total'] ?></td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+    <?php endif; ?>
 </div>
-<?php endif; ?>
