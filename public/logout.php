9962b0f39d0a8727cfd30f9816afa4bceecd5ee1
cancerogen-ge <don544@mail.ru>
2021-03-22 15:57:58 +0500

    deleteCart($user_id);

--- public/logout.php
+++ public/logout.php
@@ -8,6 +8,19 @@ ini_set('display_startup_errors', '1');
 
 session_start();
 
+require_once __DIR__ . '/../config/config.php';
+
+require_once(ENGINE_DIR . '/functions.php');
+require_once(ENGINE_DIR . '/db_model.php');
+
+$user_id = 0;
+
+if (isset($_SESSION["user_id"]) && $_SESSION["user_id"]) {
+    $user_id = (int)$_SESSION["user_id"];
+}
+
+deleteCart($user_id);
+
 $_SESSION = [];
 
 session_destroy();
--- public/product.php
+++ public/product.php
@@ -8,7 +8,7 @@ ini_set('display_startup_errors', '1');
 
 session_start();
 
-require __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 
 require_once(ENGINE_DIR . '/functions.php');
 require_once(ENGINE_DIR . '/db_model.php');
