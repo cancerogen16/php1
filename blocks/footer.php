30aeb690742b02629395f80ff6df943061dd5e14
cancerogen-ge <don544@mail.ru>
2021-03-28 14:43:17 +0500

    <div id="modal" class="modal">

--- blocks/footer.php
+++ blocks/footer.php
@@ -3,6 +3,15 @@
         <h3><?php echo date('Y'); ?></h3>
     </div>
 </footer>
+<div id="modalimg" class="modal">
+    <span class="close">&times;</span>
+    <div class="modal-content"><img class="modal-img" id="img01"></div>
+    <div id="caption"></div>
+</div>
+<div id="modal" class="modal">
+    <span class="close">&times;</span>
+    <div class="modal-content"></div>
+</div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="js/app.js"></script>
