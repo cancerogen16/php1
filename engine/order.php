6fddd7049568a117df6205e935f7b0ff3a01c311
cancerogen-ge <don544@mail.ru>
2021-03-28 14:39:53 +0500

    getOrderProducts($order_id)

--- engine/order.php
+++ engine/order.php
@@ -45,21 +45,48 @@ function getOrder($order_id) {
     $order = get_db_row($query);
 
     if (!empty($order)) {
-        $query = "SELECT name FROM `order_status` WHERE order_status_id = '" . (int)$order['order_status_id'] . "'";
-        $status = get_db_row($query)['name'];
+        $order['status'] = getOrderStatus($order['order_status_id']);
 
-        $query = "SELECT * FROM `order` WHERE order_id = '" . (int)$order_id . "'";
+        $query = "SELECT * FROM `order_item` WHERE order_id = '" . (int)$order_id . "'";
 
-        $order = get_db_row($query);
+        $order['order_products'] = get_db_result($query);
     }
 
     return $order;
 }
 
-function editOrder($order_id) {
+function getOrderProducts($order_id) {
     require_once(__DIR__ . '/../config/db.php');
 
+    $query = "SELECT * FROM `order_item` WHERE order_id = '" . (int)$order_id . "'";
 
+    return get_db_result($query);
+}
+
+function editOrder($order_id, $data) {
+    require_once(__DIR__ . '/../config/db.php');
+
+    $query = "UPDATE `order` SET username = '" . protect($data['username']) . "', phone = '" . protect($data['phone']) . "', address = '" . protect($data['address']) . "' WHERE order_id = '" . (int)$order_id . "'";
+    update_db($query);
+
+    $query = "DELETE FROM `order_item` WHERE order_id = '" . (int)$order_id . "'";
+    update_db($query);
+
+    if (!empty($data['product'])) {
+        $total = 0;
+
+        foreach ($data['product'] as $product_id => $product) {
+            $product_total = floatval($product['price']) * intval($product['quantity']);
+            $total += $product_total;
+
+            $query = "INSERT INTO `order_item` (order_id, product_id, name, quantity, price, total) VALUES ('" . (int)$order_id . "', '" . (int)$product_id . "', '" . protect($product['name']) . "', '" . intval($product['quantity']) . "', '" . floatval($product['price']) . "', '" . $product_total . "')";
+
+            update_db($query);
+        }
+
+        $query = "UPDATE `order` SET total = '" . (float)$total . "' WHERE order_id = '" . (int)$order_id . "'";
+        update_db($query);
+    }
 
     return update_db($query);
 }
@@ -78,8 +105,8 @@ function deleteOrder($order_id) {
 function validateOrder($data = []) {
     $errors = [];
 
-    if (trim($data['name']) == '') {
-        $errors['name'] = 'Название товара обязательно!';
+    if (trim($data['username']) == '') {
+        $errors['username'] = 'Название товара обязательно!';
     }
 
     return $errors;
